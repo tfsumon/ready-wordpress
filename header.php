efffec85c879089f603366aa972e9495ba40637b
Muhammad Sumon <114845583+tfsumon@users.noreply.github.com>
2022-10-17 12:37:01 +0600

    Update header.php

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- theme meta -->
+<meta name="theme-name" content="ready-wordpress" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
